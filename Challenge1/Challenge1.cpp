eb832cf6cdaf72607dea0dc610402e7741880fed
TokanoYK <umikaze0217@hotmail.co.jp>
2014-05-17 13:04:45 +0900

    ３D数学九九表示

--- Challenge1/Challenge1.cpp
+++ Challenge1/Challenge1.cpp
@@ -1,16 +1,76 @@
-// Challenge1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
-//
-
+//	３Ｄ数学課題九九の表示
 #include "stdafx.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//	３Ｄ数学課題九九
-	int a;
-	int b;
+	
+	//int a,b;
+
+	//	１から９の段までの表示
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	1
+		printf("%d ", i);
+	}
+	printf("\n");
+
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	2
+		printf("%d ", i*2);
+	}
+	printf("\n");
+
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	3
+		printf("%d ", i*3);
+	}
+	printf("\n");
 
 
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	4
+		printf("%d ", i*4);
+	}
+	printf("\n");
+
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	5
+		printf("%d ", i*5);
+	}
+	printf("\n");
+
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	6
+		printf("%d ", i*6);
+	}
+	printf("\n");
+
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	7
+		printf("%d ", i*7);
+	}
+	printf("\n");
+
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	8
+		printf("%d ", i*8);
+	}
+	printf("\n");
+
+	for(int i = 1; i < 10 ; i++)
+	{
+		//	9
+		printf("%d ", i*9);
+	}
+	printf("\n");
 
 	return 0;
 }
