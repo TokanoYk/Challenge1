1d5b5efeffa63b039181ff38382ef8121ce33e0b
TokanoYK <umikaze0217@hotmail.co.jp>
2014-05-17 14:06:05 +0900

    ３D数学の課題

---
+++ Challenge1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//	‚R‚c”Šw‚Ì‰Û‘è
+int main()
+{
+	printf("<!DOCTYPE html1>\n");
+	printf("<mata charset=\"UTF-8\">\n");
+	printf("<title>99</title>\n");
+	printf("<hi>99</hi>\n");
+	printf("<table>\n");
+	
+	printf("<th>");
+	for(int i = 1; i < 10; i++)
+	{
+		printf("<th>%2d\n"i);
+
+
+
+	}
+	return 0;
+}
